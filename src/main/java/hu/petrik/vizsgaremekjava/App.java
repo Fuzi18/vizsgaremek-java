3405083b6e916a412c8daf375ed4a46b7a1d2627
Fuzi18 <fuzesi.t2003@gmail.com>
2023-04-19 22:03:48 +0200

    shopview

--- src/main/java/hu/petrik/vizsgaremekjava/App.java
+++ src/main/java/hu/petrik/vizsgaremekjava/App.java
@@ -11,7 +11,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("shop-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
+        Scene scene = new Scene(fxmlLoader.load(), 1100, 700);
         stage.setTitle("Shops");
         stage.setScene(scene);
         stage.show();
