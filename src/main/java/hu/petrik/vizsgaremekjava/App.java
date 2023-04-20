99aa25cedc23a6deae908864d04e3205114e4cb3
Fuzi18 <fuzesi.t2003@gmail.com>
2023-04-20 09:19:38 +0200

    vezerloegysegview

--- src/main/java/hu/petrik/vizsgaremekjava/App.java
+++ src/main/java/hu/petrik/vizsgaremekjava/App.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("kaszni-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("vezerloegyseg-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1100, 700);
         stage.setTitle("Motors");
         stage.setScene(scene);
